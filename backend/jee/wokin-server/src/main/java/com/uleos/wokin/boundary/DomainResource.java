5c4f6633542b9dc5bc6079c8f709423a99b1dbd8
niki <niki@theliferocks.com>
2017-01-14 19:14:15 +0100

    add dummy json array as return

--- backend/jee/wokin-server/src/main/java/com/uleos/wokin/boundary/DomainResource.java
+++ backend/jee/wokin-server/src/main/java/com/uleos/wokin/boundary/DomainResource.java
@@ -1,6 +1,8 @@
 package com.uleos.wokin.boundary;
 
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArray;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -10,7 +12,12 @@ import javax.ws.rs.core.Response;
 public class DomainResource {
 	@GET
 	public Response getDomains() {
-		return null;
+
+		JsonArray domains = Json.createArrayBuilder()
+				.add(Json.createObjectBuilder().add("id", "10").add("name", "domain1.tld"))
+				.add(Json.createObjectBuilder().add("id", "20").add("number", "domain2.tld")).build();
+
+		return Response.ok(domains).build();
 	}
 
 }
